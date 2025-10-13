e44f7d24227d4de7671f2994e24a7541d7e6c25d
Przemek <codingbeebee@gmail.com>
2025-10-13 19:09:40 +0200

    new file:   src/assets/img/boat.jpg 	new file:   src/assets/img/fog.jpg 	new file:   src/assets/img/forest.jpg 	new file:   src/assets/img/sailboat.jpg 	new file:   src/assets/img/sunrise.jpg 	modified:   src/components/organism/HeroSection/HeroSection.module.scss 	modified:   src/components/organism/HeroSection/HeroSection.tsx

--- src/components/organism/HeroSection/HeroSection.tsx
+++ src/components/organism/HeroSection/HeroSection.tsx
@@ -6,7 +6,41 @@ import styles from './HeroSection.module.scss'
 const HeroSection = () => {
 	return (
 		<section id="hero" className={styles.homeContainer}>
-			<div className={styles.sliderContainer}></div>
+			<div className={styles.sliderContainer}>
+				<div className={styles.sliderListContainer} draggable>
+					<div className={styles.sliderList}>
+						<div className={`${styles.sliderHero} ${styles.sliderFirst}`}>
+							<div className={`${styles.sliderHeroBg} ${styles.sliderHeroFirst}`}></div>
+							<div></div>
+						</div>
+						<div className={`${styles.sliderHero} ${styles.sliderSecond}`}>
+							<div className={`${styles.sliderHeroBg} ${styles.sliderHeroSecond}`}></div>
+							<div></div>
+						</div>
+						<div className={`${styles.sliderHero} ${styles.sliderThird}`}>
+							<div className={`${styles.sliderHeroBg} ${styles.sliderHeroThird}`}></div>
+							<div></div>
+						</div>
+					</div>
+				</div>
+				<ul className={styles.dots} role="tablist">
+					<li role="presentation">
+						<button type="button" role="tab" aria-controls="slick-slide-00" aria-label="1 of 3" tabIndex={0}>
+							01
+						</button>
+					</li>
+					<li role="presentation">
+						<button type="button" role="tab" aria-controls="slick-slide-01" aria-label="2 of 3" tabIndex={-1}>
+							02
+						</button>
+					</li>
+					<li role="presentation">
+						<button type="button" role="tab" aria-controls="slick-slide-02" aria-label="3 of 3" tabIndex={-1}>
+							03
+						</button>
+					</li>
+				</ul>
+			</div>
 			<div className={styles.socialContainer}>
 				<p className={styles.socialText}>Follow</p>
 				<span className={styles.lineThrough}></span>
