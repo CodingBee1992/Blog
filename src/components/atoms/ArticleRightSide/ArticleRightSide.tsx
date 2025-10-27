ae95d0c5b3604163843585ac0a4b916a164c1455
Przemek <codingbeebee@gmail.com>
2025-10-27 20:17:25 +0100

    modified:   src/components/atoms/ArticleRightSide/ArticleRightSide.tsx 	modified:   src/components/modules/ArticleText/ArticleText.module.scss 	modified:   src/components/organism/ArticleContent/ArticleContent.module.scss

--- src/components/atoms/ArticleRightSide/ArticleRightSide.tsx
+++ src/components/atoms/ArticleRightSide/ArticleRightSide.tsx
@@ -1,9 +1,39 @@
+import { usePostContext } from '../../../hooks/usePostContext'
+import postData from '../../../utils/postData'
+import AnchorLink from '../AnchorLink/AnchorLink'
+
 interface ArticleRightSideProps {
 	styles: { [key: string]: string }
 }
 
 const ArticleRightSide = ({ styles }: ArticleRightSideProps) => {
-	return <div className={styles.articleRightSideContainer}>ArticleRightSide</div>
+	const { id, href } = usePostContext()
+
+	const prevID = id === postData[0].id ? id : id - 1
+	const nextID = id === postData[postData.length - 1].id ? id : id + 1
+
+	const prevPost = postData.find(item => item.id === prevID)
+	const nextPost = postData.find(item => item.id === nextID)
+
+	console.log(nextID)
+	console.log(postData[postData.length - 1].id)
+
+	return (
+		<div className={styles.articleRightSideContainer}>
+			<div className={`${styles.prev} ${prevID === postData[0].id ? styles.disabled : ''} `}>
+				<span>Previous</span>
+				<AnchorLink href={`${href}/?id=${prevID}`} rel="prev">
+					{prevID !== postData[0].id ? prevPost?.title : '-----'}
+				</AnchorLink>
+			</div>
+			<div className={`${styles.next} ${nextID === id ? styles.disabled : ''}`}>
+				<span>Next</span>
+				<AnchorLink href={`${href}/?id=${nextID}`} rel="next">
+					{nextID !== id ? nextPost?.title : '-----'}
+				</AnchorLink>
+			</div>
+		</div>
+	)
 }
 
 export default ArticleRightSide
