4ec896e34b59b8635140232c70bb43346121c269
Przemek <codingbeebee@gmail.com>
2026-02-15 17:39:19 +0100

    modified:   index.html 	modified:   src/App.scss 	modified:   src/assets/styles/_variable.scss 	modified:   src/components/atoms/SliderDots/SliderDots.tsx 	modified:   src/components/organism/HeroSection/HeroSection.module.scss 	modified:   src/containers/Footer/Footer.module.scss 	modified:   src/containers/Footer/Footer.tsx 	modified:   vite.config.ts

--- src/components/atoms/SliderDots/SliderDots.tsx
+++ src/components/atoms/SliderDots/SliderDots.tsx
@@ -10,7 +10,7 @@ interface SliderDotsProps {
 
 const SliderDots = ({styles,number,index,handleSwipeOnClickDots}:SliderDotsProps) => {
 	return (
-		<li  className={`${styles.dot} ${number === index ? styles.slickActive : ''} `} tabIndex={index} onClick={(e)=>handleSwipeOnClickDots(e,index)}>
+		<li  className={`${styles.dot} ${number === index ? styles.slickActive : ''} `} onClick={(e)=>handleSwipeOnClickDots(e,index)}>
 			<button
 				type="button"
 				className={styles.dotBtn}
